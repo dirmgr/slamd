927ce1b5ceb36a9f59adbd10b61492141d1bfb0a
Neil A. Wilson <nawilson@nawilson.com>
2019-03-17 23:55:51 -0500

    Add SLAMD server start and stop scripts
    Added start-slamd-server.sh and stop-slamd-server.sh shell scripts
    in the SLAMD server root directory that make it more obvious how to
    start and stop the SLAMD server.  These scripts simply invoke the
    Tomcat startup.sh and shutdown.sh scripts in the bin directory, but
    the new scripts are easier to find because they're in the top-level
    directory, and because it's more obvious what they do.
    
    There are also corresponding start-slamd-server.bat and
    stop-slamd-server.bat batch files for use on Windows.

---
+++ base_server/slamd/stop-slamd-server.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+###############################################################################
+#                             Sun Public License
+#
+# The contents of this file are subject to the Sun Public License Version
+# 1.0 (the "License").  You may not use this file except in compliance with
+# the License.  A copy of the License is available at http://www.sun.com/
+#
+# The Original Code is the SLAMD Distributed Load Generation Engine.
+# The Initial Developer of the Original Code is Neil A. Wilson.
+# Portions created by Neil A. Wilson are Copyright (C) 2019.
+# All Rights Reserved.
+#
+# Contributor(s):  Neil A. Wilson
+###############################################################################
+
+
+# Change to the location of this build script.
+cd `dirname $0`
+
+# Launch the bin/startup.sh script.
+./bin/shutdown.sh
